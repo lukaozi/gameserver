58c9a654600e002450fe81d38841c4485e164347
lucas2 <lukaozi@163.com>
2018-11-05 21:42:42 +0800

    protostuff

---
+++ server-core/src/main/java/lucas/net/protobuf/ProtostuffUtil.java
@@ -0,0 +1,74 @@
+package lucas.net.protobuf;
+
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+import org.springframework.objenesis.Objenesis;
+import org.springframework.objenesis.ObjenesisStd;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author lushengkao vip8
+ * 2018/11/5 21:25
+ */
+public class ProtostuffUtil {
+
+    private static final Map<Class<?>, Schema<?>> CACHED_SCHEMA = new ConcurrentHashMap<>();
+
+    private static Objenesis objenesis = new ObjenesisStd(true);
+
+    @SuppressWarnings("unchecked")
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) CACHED_SCHEMA.get(cls);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(cls);
+            CACHED_SCHEMA.put(cls, schema);
+        }
+        return schema;
+    }
+
+    /**
+     * 序列化（对象 -> 字节数组）
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> byte[] serialize(T obj) {
+        Class<T> cls = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(cls);
+            return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    /**
+     * 反序列化（字节数组 -> 对象）
+     */
+    public static <T> T deserialize(byte[] data, Class<T> cls) {
+        try {
+            T message = objenesis.newInstance(cls);
+            Schema<T> schema = getSchema(cls);
+            ProtostuffIOUtil.mergeFrom(data, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 生成对象
+     */
+    public static <T> T newInstance(Class<T> cls) {
+        try {
+            return objenesis.newInstance(cls);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+}
