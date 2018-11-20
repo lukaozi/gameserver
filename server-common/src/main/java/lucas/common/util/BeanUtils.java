10a5d30181aa61dda99f976c8d643b4f8a982245
lucas2 <lukaozi@163.com>
2018-11-20 21:39:26 +0800

    bean

--- server-common/src/main/java/lucas/common/util/BeanUtils.java
+++ server-common/src/main/java/lucas/common/util/BeanUtils.java
@@ -1,5 +1,7 @@
 package lucas.common.util;
 
+import org.springframework.util.ReflectionUtils;
+
 import java.lang.reflect.Field;
 
 /**
@@ -9,31 +11,11 @@ import java.lang.reflect.Field;
 public class BeanUtils {
 
     public static Object getFieldValue(Object obj, String fieldName) {
-        Field field = getDeclaredField(obj, fieldName);
+        Field field = ReflectionUtils.findField(obj.getClass(), fieldName);
+        if (field == null) {
+            return null;
+        }
         field.setAccessible(true);
-        try {
-            return field.get(obj);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static Field getDeclaredField(Object obj, String fieldName) {
-        Class<?> clazz = obj.getClass();
-        for (; clazz != Object.class; clazz = clazz.getSuperclass()) {
-            try {
-                return clazz.getDeclaredField(fieldName);
-            } catch (Exception ignored) {
-            }
-        }
-        return null;
-    }
-
-    public static boolean equalsValue(Object oldValue, Object newValue) {
-        if (newValue == null) {
-            return oldValue == null;
-        }
-        return newValue.equals(oldValue);
+        return ReflectionUtils.getField(field, obj);
     }
 }
